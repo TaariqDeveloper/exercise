fd203de7694d20afefa991bd9fa151793ac1766e
TaariqDeveloper <TaariqDeveloper@gmail.com>
2024-12-02 13:37:59 +0300

    java

---
+++ index.js
@@ -0,0 +1,134 @@
+// Exercise 1: Even or Odd
+// Write a program that:
+
+// Prompts the user to enter a number.
+// Checks whether the number is even or odd.
+// Displays a message in the console like:
+// "The number is even."
+// "The number is odd."
+
+// let number = parseInt(prompt("Enter A number"));
+
+// if(number % 2 ==0){
+//     alert("The number is even.");
+// }
+// else{
+//     alert("The number is odd.");
+// }
+
+
+// Exercise 3: Login Authentication
+// Write a program that:
+
+// Asks the user to enter a username and password.
+// Checks:
+// If the username is "admin" and the password is "1234", display "Login successful!".
+// Otherwise, display "Invalid username or password.".
+
+// let username = prompt("Enter your username");
+// let password = parseInt(prompt("Enter your password"))
+
+
+// if(username == "admin" && password == 1234){
+//     alert("Login successful!");
+// }
+// else{
+//     alert("Invalid username or password.");
+// }
+
+// Exercise 4: Number Comparison
+// Write a program that:
+
+// Prompts the user to enter two numbers.
+// Checks and displays whether:
+// The first number is greater.
+// The second number is greater.
+// Both numbers are equal.
+
+
+// let number1 = parseInt(prompt("Enter Number 1"));
+// let number2 = parseInt(prompt("Enter Number 1"));
+// if(number1>number2){
+//     alert("Number 1 is greater.");
+// }
+// else if(number2 > number1){
+//     alert("Number 2 is greater.");
+// }
+// else{
+//     alert("Both numbers are equal.");
+// }
+
+
+// Write a program that:
+
+// Prompts the user to enter the color of a traffic light ("red", "yellow", or "green").
+// Displays:
+// "Stop!" if the input is "red".
+// "Get Ready!" if the input is "yellow".
+// "Go!" if the input is "green".
+// "Invalid input!" for anything else.
+
+
+// let color = prompt("");
+
+
+
+// if(color == "red"){
+//     alert("Stop!");
+// }
+
+// else if(color == "yellow"){
+//     alert("Get Ready!");
+// }
+
+// else if(color == "green"){
+//     alert("Go!");
+// }
+
+// else{
+//     alert("Invalid input!");
+// }
+
+
+
+// Exercise 6: Leap Year Checker
+// Write a program that:
+
+// Prompts the user to enter a year.
+// Checks if the year is a leap year:
+// A year is a leap year if:
+// It is divisible by 4 and not divisible by 100.
+// Or, it is divisible by 400.
+// Displays whether the year is a leap year or not.
+
+
+
+
+
+// Exercise 7: Temperature Advice
+// Write a program that:
+
+// Prompts the user to enter a temperature.
+// Gives advice based on the temperature:
+// Less than 10°C: "It's cold, wear a jacket!".
+// Between 10°C and 25°C: "It's cool, enjoy your day!".
+// Above 25°C: "It's hot, stay hydrated!".
+
+
+// let discount = parseInt(prompt("enter discount"))
+// if (discount >=40){
+//     alert("You get 40% discount");
+// }
+// else if (discount >=30){
+//     alert("You get 30% discount");
+// }
+// else if (discount >=20){
+//     alert("You get 20% discount");
+// }
+// else{
+//     alert("You don't get any discount");
+// }
+
+
+
+
